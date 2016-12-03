8818139afdd8cd1798bf22d7d814241f813368c6
KatrukSergii <katruksergyi@gmail.com>
2016-12-03 11:04:16 +0200

    Server infrastructure.

---
+++ Network/Server/Interface/IServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Server.Interface
+{
+    /// <summary>
+    /// Defines the common logic for server.
+    /// </summary>
+    /// <owner>Sergii Katruk</owner>
+    public interface IServer
+    {
+        /// <summary>
+        /// Starts this instance.
+        /// </summary>
+        /// <owner>Sergii Katruk</owner>
+        void Start();
+
+        /// <summary>
+        /// Stops this instance.
+        /// </summary>
+        /// <owner>Sergii Katruk</owner>
+        void Stop();
+    }
+}
---
+++ Network/Server/TCP/Server.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Server.Interface;
+
+namespace Server.TCP
+{
+    /// <summary>
+    /// Defines the server with using TCP.
+    /// </summary>
+    /// <owner>Sergii Katruk</owner>
+    public class Server : IServer
+    {
+        /// <summary>
+        /// Starts this instance.
+        /// </summary>
+        /// <owner>Sergii Katruk</owner>
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Stops this instance.
+        /// </summary>
+        /// <owner>Sergii Katruk</owner>
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
