68461f610ab0c9b09c59aa9dc139215e03a36ea1
KatrukSergii <katruksergyi@gmail.com>
2016-12-03 11:08:59 +0200

    Client infrastructure.

---
+++ Network/Client/Interface/IClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Client.Interface
+{
+    /// <summary>
+    /// Holds the common functionality for client.
+    /// </summary>
+    /// <owner>Sergii Katruk</owner>
+    public interface IClient
+    {
+        /// <summary>
+        /// Connects this instance.
+        /// </summary>
+        /// <owner>Sergii Katruk</owner>
+        void Connect();
+        
+        /// <summary>
+        /// Disconnects this instance.
+        /// </summary>
+        /// <owner>Sergii Katruk</owner>
+        void Disconnect();
+
+        /// <summary>
+        /// Sends this instance.
+        /// </summary>
+        /// <owner>Sergii Katruk</owner>
+        void Send();
+    }
+}
---
+++ Network/Client/UDP/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Client.Interface;
+
+namespace Client.UDP
+{
+    /// <summary>
+    /// Defines logic for the client with using UDP.
+    /// </summary>
+    /// <owner>Sergii Katruk</owner>
+    public class Client : IClient
+    {
+        /// <summary>
+        /// Connects this instance.
+        /// </summary>
+        /// <owner>Sergii Katruk</owner>
+        public void Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Disconnects this instance.
+        /// </summary>
+        /// <owner>Sergii Katruk</owner>
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sends this instance.
+        /// </summary>
+        /// <owner>Sergii Katruk</owner>
+        public void Send()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
